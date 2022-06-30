87c38733d1a8742a221b1adbaa126e48a8f28649
Kaipaev Timur <kaipaevtimur55@gmail.com>
2022-06-30 21:53:42 +0200

    Initial Commit

---
+++ UIWeaterApp/UIWeaterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIWeaterAppApp.swift
+//  UIWeaterApp
+//
+//  Created by Admin on 30.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct UIWeaterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
